688145cb8d219f7f9ae0f28f9fd0aca73cc65b96
ossysek <beata.ossysek@gmail.com>
2026-06-29 10:23:34 +0000

    student

--- src/app.py
+++ src/app.py
@@ -61,6 +61,10 @@ def signup_for_activity(activity_name: str, email: str):
 
     # Get the specific activity
     activity = activities[activity_name]
+    
+    #validate if student is already signed up
+    if email in activity["participants"]:
+        raise HTTPException(status_code=400, detail="Student already signed up for this activity")
 
     # Add student
     activity["participants"].append(email)
